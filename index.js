6b7178ea0ed1f0d1bca823e4b822eb7fdebd8e97
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-11 16:25:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  return `'Its ${days} days until ${holidays}!`
+  return `It's ${days} days until ${holidays}!`
 }
